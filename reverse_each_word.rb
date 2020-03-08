9ee41e182736af508b9ab2a9bb530bb44a6cce39
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-08 10:15:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,10 @@
 
 
-def reverse_each_word("Hello, this is a string")
-
+def reverse_each_word(sentence)
+sentence = sentence.split (" ")
+new_sentence = []
+sentence.each do |word|
+new_sentence << word.reverse
+end
+new_sentence
 end
